03e481f7c0136e8a5a49d96a82ad250bcbfd90ac
d2techcomputadores-cloud <d2techcomputadores-cloud@email>
2025-11-19 16:46:31 -0300

    funcao atualizar jogo

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ const imgCookies = document.querySelector("#img-cookie")
 let valorQuantCookies = 0
 quantCookies.value = valorQuantCookies;
 
-imgCookies.addEventListener("click", atualizarQuantCookies)
+imgCookies.addEventListener("click", atualizarJogo)
 
 function atualizarQuantCookies(){
     valorQuantCookies = valorQuantCookies + 1
 
     quantCookies.value = valorQuantCookies
+}
+
+function atualizarJogo(){
+
+    atualizarQuantCookies()
+
+    //Regras do jogo 
+    if(valorQuantCookies>=10){
+        const imgCursor = document.querySelector("#img-cursor")
+        imgCursor.style.borderColor= "green";
+    }
 }
